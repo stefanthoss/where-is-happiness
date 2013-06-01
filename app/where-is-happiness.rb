c928c8b2978a59b88d475067226e6435c4dc3fd0
Stefan Thoss <stef@nthoss.de>
2013-06-01 23:50:18 +0000

    Added keyword filter

--- app/where-is-happiness.rb
+++ app/where-is-happiness.rb
@@ -9,27 +9,29 @@ config = YAML.load_file('../config.yml')
 
 set :environment, :development
 
+get '/getstreampoints' do
 client = Mysql2::Client.new(:host => config['db']['host'],
   :username => config['db']['username'],
   :password => config['db']['password'],
   :database => config['db']['database'],
   :host => config['db']['host'])
 
-get '/getstreampoints' do
   points = Hash.new
   points['tweets'] = []
-  
-if #{params[:fromid]}.nil?
-  query =  "SELECT * FROM tweets"
-else
-  query =  "SELECT * FROM tweets WHERE id > #{params[:fromid]}"
-end
 
-tweets = client.query(query)
-tweets.each do |tweet|
-  points['tweets'] << { 'lat' => tweet['lat'], 'lon' => tweet['lng'] }
-  points['lastid'] = tweet['id']
-end
+  from_id = params[:fromid].nil? ? -1 : params[:fromid]
+
+  unless params[:keyword].nil?
+    query = "SELECT * FROM tweets WHERE id > #{from_id} AND text LIKE \"%#{params[:keyword]}%\""
+  else
+    query = "SELECT * FROM tweets WHERE id > #{from_id}"
+  end
+
+  tweets = client.query(query)
+  tweets.each do |tweet|
+    points['tweets'] << { 'lat' => tweet['lat'], 'lon' => tweet['lng'] }
+    points['lastid'] = tweet['id']
+  end
 
  points.to_json
 end
