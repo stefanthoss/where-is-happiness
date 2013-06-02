9fa5dce34f9e712f38d54966849716c88ea17f97
stefanthoss <stef@nthoss.de>
2013-06-01 20:05:50 -0700

    Added slides

---
+++ app/public/slides/slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Where is Happiness?',
+    //subtitle: 'Subtitle Goes Here',
+    eventTitle: 'Google Hack for Change 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/ndochlogofacebook.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Stefan Thoss',
+    company: 'Hacker',
+    twitter: '@kabelknoten',
+    github: 'http://github.com/stefanthoss'
+  }, {
+    name: 'Bernard Van Haecke',
+    company: 'Hacker',
+    twitter: '@busyhacking',
+    github: 'http://github.com/bernardvh'
+  }]
+};
+
