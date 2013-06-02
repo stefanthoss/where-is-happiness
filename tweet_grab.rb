b88505486cb7d703f1f9531373e8a7941a0ab7e0
stefanthoss <stef@nthoss.de>
2013-06-01 17:03:00 -0700

    Added mood analyzer

--- tweet_grab.rb
+++ tweet_grab.rb
@@ -1,4 +1,5 @@
 require 'tweetstream'
+require 'afinn_analyzer'
 require 'yajl'
 require 'yaml'
 require 'mysql2'
@@ -13,11 +14,13 @@ TweetStream.configure do |twitter_config|
   twitter_config.auth_method        = :oauth
 end
 
+analyzer = AfinnAnalyzer.new "AFINN-111.txt"
+
 def format_time time
  time.nil? ?  "NULL" : "\"#{time.utc.to_s[0..18]}\""
 end
 
-# CREATE TABLE wih.tweets (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255), time DATETIME, lat DOUBLE, lng DOUBLE);
+# CREATE TABLE wih.tweets (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255), time DATETIME, lat DOUBLE, lng DOUBLE, mood_pos INTEGER, mood_neg INTEGER);
 
 client = Mysql2::Client.new(:host => config['db']['host'],
   :username => config['db']['username'],
@@ -47,12 +50,13 @@ tweetstream.locations("-122.519746,37.234702,-121.839967,37.823887") do |status|
   unless status.geo.nil?
     lat = status.geo.coordinates[0].to_f
     lng = status.geo.coordinates[1].to_f
-  else
-    lat = status.place.bounding_box.coordinates[0][0][1].to_f
-    lng = status.place.bounding_box.coordinates[0][0][0].to_f
-  end
+  # else
+  #   lat = status.place.bounding_box.coordinates[0][0][1].to_f
+  #   lng = status.place.bounding_box.coordinates[0][0][0].to_f
+  # end
+  mood = analyzer.analyze status.text
   # puts "#{status.created_at} - #{lat},#{lng} - #{status.text}"
-  query = "INSERT INTO tweets (text, time, lat, lng) VALUES (\"#{client.escape(status.text)}\", #{format_time(status.created_at)}, #{lat}, #{lng})"
+  query = "INSERT INTO tweets (text, time, lat, lng, mood_pos, mood_neg) VALUES (\"#{client.escape(status.text)}\", #{format_time(status.created_at)}, #{lat}, #{lng}, #{mood[:positive]}, #{mood[:negative]})"
   puts query
   client.query query
 end
